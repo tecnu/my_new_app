2581e2df5364c8cf3b9442ea0e723c27664b52c9
tecnu <tecnu@users.noreply.github.com>
2017-04-09 10:49:59 -0400

    4.1 first commit

---
+++ s-new-app/config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  resources :products
+  get 'static_pages/about'
+
+  get 'static_pages/contact'
+
+  get 'static_pages/index'
+
+  root 'static_pages#index'
+  
+  
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
