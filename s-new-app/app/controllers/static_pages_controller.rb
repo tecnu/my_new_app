d9e987b7f437d2cfa84f8371583685ca86fcdb94
tecnu <tecnu@users.noreply.github.com>
2017-04-10 20:06:07 -0400

    4.3 created orders controller, along with actions and views

--- s-new-app/app/controllers/static_pages_controller.rb
+++ s-new-app/app/controllers/static_pages_controller.rb
@@ -2,3 +2,9 @@ class StaticPagesController < ApplicationController
   def index
   end
 end
+
+class StaticPagesController < ApplicationController
+  def landing_page
+    @featured_product = Product.first
+  end
+end
