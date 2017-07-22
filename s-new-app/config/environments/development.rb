7f2e62a6d99a0121700923bcb148f5025ad9094b
tecnu <tecnu@users.noreply.github.com>
2017-07-22 10:23:19 -0400

    5.4 Authentication: created sign up and login pages for users

--- s-new-app/config/environments/development.rb
+++ s-new-app/config/environments/development.rb
@@ -51,4 +51,11 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  
+  # Devise - So dynamic links in emails from development env generate the right URLS
+  config.action_mailer.default_url_options = { host: 'localhost:8080' }
+  default_url_options
+  
 end
+
+  
