4b9f1daa97d4c7c46a968f1233808d9e8923c26b
tecnu <tecnu@users.noreply.github.com>
2017-09-02 18:48:51 -0400

    6.11 Added caching; including Memcache and Dalli gem

--- s-new-app/config/environments/production.rb
+++ s-new-app/config/environments/production.rb
@@ -94,4 +94,15 @@ Rails.application.configure do
   # Added to correct elevateZoom js plugin not showing in Heroku
   config.serve_static_assets = true
   
+  # Use a different cache store in production.
+  config.cache_store = :dalli_store,
+                    (ENV["MEMCACHIER_SERVERS"] || "").split(","),
+                    {:username => ENV["MEMCACHIER_USERNAME"],
+                     :password => ENV["MEMCACHIER_PASSWORD"],
+                     :failover => true,
+                     :socket_timeout => 1.5,
+                     :socket_failure_delay => 0.2,
+                     :down_retry_delay => 60
+                    }
+  
 end
