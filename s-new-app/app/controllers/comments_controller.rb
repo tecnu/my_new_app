07fcfb238d8008abe5d9314014411e652a56095b
tecnu <tecnu@users.noreply.github.com>
2017-09-07 22:32:45 -0400

    6.14: ActiveJob and Advanced ActionCable

--- s-new-app/app/controllers/comments_controller.rb
+++ s-new-app/app/controllers/comments_controller.rb
@@ -8,7 +8,8 @@ class CommentsController < ApplicationController
     @user = current_user
     respond_to do |format|
       if @comment.save
-        ActionCable.server.broadcast 'product_channel', comment: @comment, average_rating: @comment.product.average_rating
+        #ActionCable.server.broadcast 'product_channel', comment: @comment, average_rating: @comment.product.average_rating
+        ProductChannel.broadcast_to @product.id, comment: CommentsController.render(partial: 'comments/comment', locals: {comment: @comment, current_user: current_user}), average_rating: @product.average_rating
         format.html { redirect_to @product, notice: 'Review was created successfully.' }
         format.json { render :show, status: :created, location: @product }
         format.js
@@ -16,7 +17,7 @@ class CommentsController < ApplicationController
         format.html { redirect_to @product, alert: 'Review was not saved successfully.' }
         format.json { render json: @comment.errors, status: :unprocessable_entity }
       end
-    end
+    end 
   end
 
   def destroy
