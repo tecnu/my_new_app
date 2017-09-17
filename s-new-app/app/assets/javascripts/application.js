f2e182fbe2293289d05263d88b8a2529d302e9f1
tecnu <tecnu@users.noreply.github.com>
2017-09-17 12:42:35 -0400

    6.15 Revision

--- s-new-app/app/assets/javascripts/application.js
+++ s-new-app/app/assets/javascripts/application.js
@@ -13,5 +13,4 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
-//= require jquery.elevateZoom-3.0.8.min.js
 //= require turbolinks
--- s-new-app/app/models/product.rb
+++ s-new-app/app/models/product.rb
@@ -17,12 +17,4 @@ def average_rating
   comments.average(:rating).to_f
 end
 
-def views
-  $redis.get("product:#{id}")
-end
-
-def viewed!
-  $redis.incr("product:#{id}")
-end
-
 end
